20841713327085b5b2faf9ccdc553de3992e8a0d
Caroline Jepps <carolinejepps@Carolines-MacBook-Air.local>
2020-08-20 11:29:52 -0700

    Added table and db call for prod and cat stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+  password: ENV["HTTP_BASIC_PASSWORD"]
+
   def show
+    @prod_stats = Product.count
+    @cat_stats = Category.count
   end
+
 end
